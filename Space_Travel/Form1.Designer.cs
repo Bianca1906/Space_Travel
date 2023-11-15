c99d6b89c04e1111a60b1682160a424d6f58665c
Bianca <Bianca@DESKTOP-LJTAESB>
2023-11-15 22:43:43 +0200

    Add project files.

---
+++ Space_Travel/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace Space_Travel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.b1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // b1
+            // 
+            this.b1.AutoSize = true;
+            this.b1.BackColor = System.Drawing.Color.Transparent;
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.ForeColor = System.Drawing.Color.Black;
+            this.b1.Image = global::Space_Travel.Properties.Resources.nava_removebg_preview2;
+            this.b1.Location = new System.Drawing.Point(1046, 410);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(70, 31);
+            this.b1.TabIndex = 1;
+            this.b1.Text = ".......";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = global::Space_Travel.Properties.Resources.fundal_dim_planete_2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1384, 761);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Space travel";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label b1;
+    }
+}
+
---
+++ Space_Travel/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Space_Travel
+{
+    public partial class Form1 : Form
+    {
+        int poz, raza, cx, cy;
+        int[,] coord;
+        int nr = 9;
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        public Form1()
+        {
+            double unghi = 360.0 / nr, ur = 3.1415 * unghi / 180;
+            InitializeComponent();
+            poz = 0;
+            raza = 325; 
+            coord = new int[nr, 2]; 
+            cx = this.Width / 2 -60;  
+            cy = this.Height / 2;
+            for (int i = 0; i < nr; i++)
+            {
+                coord[i, 0] = (int)(cx + raza * Math.Sin(i * ur));
+                coord[i, 1] = (int)(cy + raza * Math.Cos(i * ur));
+            }
+            b1.Top = coord[poz, 1];
+            b1.Left = coord[poz, 0];
+            this.MouseWheel += MyMouseWheel;
+        }
+        private void MyMouseWheel(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Delta > 0) poz++;
+            else poz--;
+            poz = (poz + nr) % nr; 
+            b1.Top = coord[poz, 1];
+            b1.Left = coord[poz, 0];
+            //Form1.ActiveForm.Text = "x= " + coord[poz, 0].ToString()+" "+"y= " + coord[poz, 0].ToString();
+           // Form1.ActiveForm.Invalidate();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
