eb16b9fd4b73a20efa2a40e7259e0f158385a105
Bianca Ruxandra <bianca.batrina196@gmail.com>
2023-11-19 01:00:44 +0200

    meniu_marte

--- Space_Travel/Form2.Designer.cs
+++ Space_Travel/Form2.Designer.cs
@@ -29,35 +29,29 @@ namespace Space_Travel
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Marte));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.numeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.caracteristiciFiziceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.structurăInternăToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.geologieDeSuprafațăToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.solToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.atmosferăToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.metanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.auroraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.climăToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.orbităȘiRotațieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.satelițiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.explorareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.viitorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.observațiiIstoriceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.observațiiAnticeȘiMedievaleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.canaleMarțieneToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.istoricExplorareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.posibilitateaExistențeiViețiiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pozeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.numeToolStripMenuItem,
-            this.caracteristiciFiziceToolStripMenuItem,
-            this.explorareToolStripMenuItem,
-            this.observațiiIstoriceToolStripMenuItem});
+            this.posibilitateaExistențeiViețiiToolStripMenuItem,
+            this.pozeToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(984, 24);
@@ -67,121 +61,97 @@ namespace Space_Travel
             // numeToolStripMenuItem
             // 
             this.numeToolStripMenuItem.Name = "numeToolStripMenuItem";
-            this.numeToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
-            this.numeToolStripMenuItem.Text = "Nume";
+            this.numeToolStripMenuItem.Size = new System.Drawing.Size(87, 20);
+            this.numeToolStripMenuItem.Text = "Istoric Nume";
+            this.numeToolStripMenuItem.Click += new System.EventHandler(this.numeToolStripMenuItem_Click);
             // 
-            // caracteristiciFiziceToolStripMenuItem
+            // button1
             // 
-            this.caracteristiciFiziceToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.structurăInternăToolStripMenuItem,
-            this.geologieDeSuprafațăToolStripMenuItem,
-            this.solToolStripMenuItem,
-            this.atmosferăToolStripMenuItem,
-            this.climăToolStripMenuItem,
-            this.orbităȘiRotațieToolStripMenuItem,
-            this.satelițiToolStripMenuItem});
-            this.caracteristiciFiziceToolStripMenuItem.Name = "caracteristiciFiziceToolStripMenuItem";
-            this.caracteristiciFiziceToolStripMenuItem.Size = new System.Drawing.Size(117, 20);
-            this.caracteristiciFiziceToolStripMenuItem.Text = "Caracteristici fizice";
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Font = new System.Drawing.Font("Sigmar One", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(958, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(26, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "❌";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
             // 
-            // structurăInternăToolStripMenuItem
+            // label2
             // 
-            this.structurăInternăToolStripMenuItem.Name = "structurăInternăToolStripMenuItem";
-            this.structurăInternăToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
-            this.structurăInternăToolStripMenuItem.Text = "Structură internă";
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Orbitron", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(488, 1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Planeta Marte";
             // 
-            // geologieDeSuprafațăToolStripMenuItem
+            // textBox1
             // 
-            this.geologieDeSuprafațăToolStripMenuItem.Name = "geologieDeSuprafațăToolStripMenuItem";
-            this.geologieDeSuprafațăToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
-            this.geologieDeSuprafațăToolStripMenuItem.Text = "Geologie de suprafață";
+            this.textBox1.BackColor = System.Drawing.Color.LightGray;
+            this.textBox1.Font = new System.Drawing.Font("Doppio One", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(12, 291);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(491, 343);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Visible = false;
             // 
-            // solToolStripMenuItem
+            // posibilitateaExistențeiViețiiToolStripMenuItem
             // 
-            this.solToolStripMenuItem.Name = "solToolStripMenuItem";
-            this.solToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
-            this.solToolStripMenuItem.Text = "Sol";
+            this.posibilitateaExistențeiViețiiToolStripMenuItem.Name = "posibilitateaExistențeiViețiiToolStripMenuItem";
+            this.posibilitateaExistențeiViețiiToolStripMenuItem.Size = new System.Drawing.Size(164, 20);
+            this.posibilitateaExistențeiViețiiToolStripMenuItem.Text = "Posibilitatea existenței vieții";
+            this.posibilitateaExistențeiViețiiToolStripMenuItem.Click += new System.EventHandler(this.posibilitateaExistențeiViețiiToolStripMenuItem_Click);
             // 
-            // atmosferăToolStripMenuItem
+            // textBox2
             // 
-            this.atmosferăToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.metanToolStripMenuItem,
-            this.auroraToolStripMenuItem});
-            this.atmosferăToolStripMenuItem.Name = "atmosferăToolStripMenuItem";
-            this.atmosferăToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
-            this.atmosferăToolStripMenuItem.Text = "Atmosferă";
+            this.textBox2.BackColor = System.Drawing.Color.LightGray;
+            this.textBox2.Font = new System.Drawing.Font("Doppio One", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.Black;
+            this.textBox2.Location = new System.Drawing.Point(12, 291);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(795, 343);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Visible = false;
             // 
-            // metanToolStripMenuItem
+            // imageList1
             // 
-            this.metanToolStripMenuItem.Name = "metanToolStripMenuItem";
-            this.metanToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.metanToolStripMenuItem.Text = "Metan";
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "poza1.jpg");
+            this.imageList1.Images.SetKeyName(1, "poza2.jpeg");
+            this.imageList1.Images.SetKeyName(2, "poza3.jpg");
+            this.imageList1.Images.SetKeyName(3, "poza4.jpg");
+            this.imageList1.Images.SetKeyName(4, "poza5.jpg");
             // 
-            // auroraToolStripMenuItem
+            // pictureBox1
             // 
-            this.auroraToolStripMenuItem.Name = "auroraToolStripMenuItem";
-            this.auroraToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.auroraToolStripMenuItem.Text = "Aurora";
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(673, 266);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
             // 
-            // climăToolStripMenuItem
+            // timer1
             // 
-            this.climăToolStripMenuItem.Name = "climăToolStripMenuItem";
-            this.climăToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
-            this.climăToolStripMenuItem.Text = "Climă";
+            this.timer1.Interval = 600;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // orbităȘiRotațieToolStripMenuItem
+            // pozeToolStripMenuItem
             // 
-            this.orbităȘiRotațieToolStripMenuItem.Name = "orbităȘiRotațieToolStripMenuItem";
-            this.orbităȘiRotațieToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
-            this.orbităȘiRotațieToolStripMenuItem.Text = "Orbită și rotație";
-            // 
-            // satelițiToolStripMenuItem
-            // 
-            this.satelițiToolStripMenuItem.Name = "satelițiToolStripMenuItem";
-            this.satelițiToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
-            this.satelițiToolStripMenuItem.Text = "Sateliți";
-            // 
-            // explorareToolStripMenuItem
-            // 
-            this.explorareToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.viitorToolStripMenuItem,
-            this.istoricExplorareToolStripMenuItem});
-            this.explorareToolStripMenuItem.Name = "explorareToolStripMenuItem";
-            this.explorareToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
-            this.explorareToolStripMenuItem.Text = "Explorare";
-            // 
-            // viitorToolStripMenuItem
-            // 
-            this.viitorToolStripMenuItem.Name = "viitorToolStripMenuItem";
-            this.viitorToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.viitorToolStripMenuItem.Text = "Viitor";
-            // 
-            // observațiiIstoriceToolStripMenuItem
-            // 
-            this.observațiiIstoriceToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.observațiiAnticeȘiMedievaleToolStripMenuItem,
-            this.canaleMarțieneToolStripMenuItem});
-            this.observațiiIstoriceToolStripMenuItem.Name = "observațiiIstoriceToolStripMenuItem";
-            this.observațiiIstoriceToolStripMenuItem.Size = new System.Drawing.Size(113, 20);
-            this.observațiiIstoriceToolStripMenuItem.Text = "Observații istorice";
-            // 
-            // observațiiAnticeȘiMedievaleToolStripMenuItem
-            // 
-            this.observațiiAnticeȘiMedievaleToolStripMenuItem.Name = "observațiiAnticeȘiMedievaleToolStripMenuItem";
-            this.observațiiAnticeȘiMedievaleToolStripMenuItem.Size = new System.Drawing.Size(230, 22);
-            this.observațiiAnticeȘiMedievaleToolStripMenuItem.Text = "Observații antice și medievale";
-            // 
-            // canaleMarțieneToolStripMenuItem
-            // 
-            this.canaleMarțieneToolStripMenuItem.Name = "canaleMarțieneToolStripMenuItem";
-            this.canaleMarțieneToolStripMenuItem.Size = new System.Drawing.Size(230, 22);
-            this.canaleMarțieneToolStripMenuItem.Text = "„Canale” marțiene";
-            // 
-            // istoricExplorareToolStripMenuItem
-            // 
-            this.istoricExplorareToolStripMenuItem.Name = "istoricExplorareToolStripMenuItem";
-            this.istoricExplorareToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.istoricExplorareToolStripMenuItem.Text = "Istoric explorare";
+            this.pozeToolStripMenuItem.Name = "pozeToolStripMenuItem";
+            this.pozeToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.pozeToolStripMenuItem.Text = "Poze";
+            this.pozeToolStripMenuItem.Click += new System.EventHandler(this.pozeToolStripMenuItem_Click);
             // 
             // Marte
             // 
@@ -190,13 +160,20 @@ namespace Space_Travel
             this.BackgroundImage = global::Space_Travel.Properties.Resources.martee;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(984, 661);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.textBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Marte";
             this.Text = "Marte";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -206,21 +183,14 @@ namespace Space_Travel
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem numeToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem caracteristiciFiziceToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem structurăInternăToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem geologieDeSuprafațăToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem solToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem atmosferăToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem metanToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem auroraToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem climăToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem orbităȘiRotațieToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem satelițiToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem explorareToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem viitorToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem istoricExplorareToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem observațiiIstoriceToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem observațiiAnticeȘiMedievaleToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem canaleMarțieneToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem posibilitateaExistențeiViețiiToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem pozeToolStripMenuItem;
     }
 }
--- Space_Travel/Form2.cs
+++ Space_Travel/Form2.cs
@@ -16,5 +16,72 @@ namespace Space_Travel
         {
             InitializeComponent();
         }
+        int i;
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Red;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.White;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void numeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            pictureBox1.Visible = false;
+            textBox2.Visible = false;
+            textBox1.Visible = true;
+            textBox1.Text = "Planeta este numită după zeul roman al războiului, Mars, o asociere făcută " +
+                "datorită culorii sale roșiatice care sugerează sângele. În limba greacă, " +
+                "planeta este cunoscută sub numele de Ἄρης Arēs, cu rădăcina inflexională Ἄρε- Are-." +
+                " De aici rezultă termeni tehnici, precum areologie (geologia lui Marte) și numele stelei Antares." +
+                "„Mars” este, de asemenea, baza numelui lunii martie(din latinescul Martius mēnsis „luna lui Marte”)";
+
+        }
+
+        private void posibilitateaExistențeiViețiiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            pictureBox1.Visible = false;
+            textBox1.Visible = false;
+            textBox2.Visible = true;
+            textBox2.Text = "Există cercetări în curs de desfășurare care evaluează potențialul de locuibilitate din trecut al lui Marte, " +
+                "precum și posibilitatea existenței vieții. Sunt planificate viitoare misiuni de astrobiologie," +
+                " cum ar fi roverul Rosalind Franklin al Agenției Spațiale Europene. Apa lichidă nu poate exista pe suprafața planetei Marte " +
+                "din cauza presiunii atmosferice scăzute, care este mai mică de 1% din presiunea atmosferică pe Terra, cu excepția unor mici creșteri " +
+                "pentru perioade scurte. Cele două calote glaciare polare par a fi făcute în mare parte din apă. Volumul de apă din calota glaciară" +
+                " a Polului Sud, dacă este topită, ar fi suficient pentru a acoperi întreaga suprafață planetară până la o adâncime de 11 metri." +
+                " În noiembrie 2016, NASA a raportat găsirea unei cantități mari de gheață subterană în regiunea Utopia Planitia de pe Marte. " +
+                "Se estimează că volumul de apă detectat este echivalent cu volumul de apă din Lacul Superior.";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            if (i <= 4)
+            {
+                pictureBox1.Image = imageList1.Images[i];
+                i++;
+            }
+           
+        }
+
+        private void pozeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            pictureBox1.Visible = true;
+            timer1.Enabled = true;
+            i = 0;
+            
+        }
     }
 }
